4b265fc0f004d0e5adb3c1ba7c340d6e6d586ebe
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-09-08 21:11:07 -0500

    Fixed remove back method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -107,10 +107,23 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO
-		Fix this method
-	*/
-
+	if(!isEmpty())
+	{
+		lastNode = m_front;
+		while(lastNode->getNext() != nullptr)
+		{
+			secondintoLast = lastNode;
+			lastNode = lastNode->getNext();
+		}
+		delete lastNode;
+		secondintoLast->setNext(nullptr);
+		m_size--;
+		isRemoved = true;
+	}
+	else
+	{
+		isRemoved = false;
+	}
 	return(isRemoved);
 }
 
